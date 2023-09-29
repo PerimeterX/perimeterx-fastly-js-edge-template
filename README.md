d125c2da3cfa83bd71ec7c91dcb18ef398fcae15
Ori <ori.gold@perimeterx.com>
2023-09-28 20:57:41 -0700

    simplifying example a little

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # HUMAN Security Starter Kit for Fastly Compute@Edge JavaScript
 
-See the official documentation [here]().
+See the official documentation [here](https://docs.perimeterx.com/docs/installation-fastly-js).
--- src/index.js
+++ src/index.js
@@ -10,18 +10,12 @@ const params = {
 
 // define what to do when requests pass HUMAN enforcement
 const onPass = (event) => {
-    console.log('handling HUMAN-validated request')
-    return fetch(event.request, { backend: 'origin' })
+    // Add your event handling logic here, for example:
+    // return fetch(event.request, { backend: 'origin' });
 };
 
-// define what to do for block responses (optional)
-const onResponse = (response) => {
-    console.log('handling response from HUMAN enforcer');
-    return response;
-};
-
-// create request handler
-const handleRequest = createEnforcedRequestHandler(params, onPass, onResponse);
+// create request handler with configuration and onPass callback
+const handleRequest = createEnforcedRequestHandler(params, onPass);
 
 // invoke handleRequest on incoming fetch event
 addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
